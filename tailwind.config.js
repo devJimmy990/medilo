a66460af573d6f94d7e3f6c7568e14dc9734f94d
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2024-12-05 01:20:28 +0200

    setup v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'heading': ['Poppins', ...defaultTheme.fontFamily.sans],
+        'body': ['Rubik', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        white: '#fff',
+        black: '#000',
+        heading: '#222',
+        body: '#636363',
+        accent: '#2ea6f7',
+        blue: '#002261',
+        gray: '#e8edf0',
+        border: '#dddddd',
+      },
+    },
   },
   plugins: [],
 }
