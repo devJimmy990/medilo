6e5a883e48fd3099a909ad76a673ebbe8f80f0ac
Eslam Fayez <eslamfayez34@gmail.com>
2024-12-08 17:45:24 +0200

    Initial commit

---
+++ src/scripts/detaildoctor.js
@@ -0,0 +1,36 @@
+onload = () => {
+  let progress = document.querySelectorAll(".progress .style-background");
+  let progress_container = document.querySelector(".progress-container");
+
+  window.addEventListener("scroll", () => {
+    if (progress_container.offsetHeight <= window.scrollY) {
+      progress.forEach((ele) => {
+        ele.style.width = ele.getAttribute("data-set");
+      });
+    }
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+    },
+  });
+
+}
--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,4 +1,5 @@
 onload = () => {
+<<<<<<< HEAD
   const menu = document.getElementById("menu");
   const close = document.getElementById("close");
   const modal = document.getElementById("modal");
@@ -11,3 +12,17 @@ onload = () => {
     modal.classList.replace("hidden", "flex");
   });
 };
+=======
+    const menu = document.getElementById('menu');
+    const close = document.getElementById('close');
+    const modal = document.getElementById('modal');
+
+    close.addEventListener('click', () => {
+        modal.classList.replace('flex', 'hidden');
+    })
+
+    menu.addEventListener('click', () => {
+        modal.classList.replace('hidden', 'flex');
+    })
+}
+>>>>>>> 27e18ffd775d21717411ed30844f779f7d6aea20
