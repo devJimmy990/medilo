23c6aeaf0338ad88f2f7222cce727fdc82f32213
Eslam Fayez <eslamfayez34@gmail.com>
2024-12-08 17:37:15 +0200

    Initial commit

---
+++ src/scripts/nav.js
@@ -0,0 +1,13 @@
+onload = () => {
+  const menu = document.getElementById("menu");
+  const close = document.getElementById("close");
+  const modal = document.getElementById("modal");
+
+  close.addEventListener("click", () => {
+    modal.classList.replace("flex", "hidden");
+  });
+
+  menu.addEventListener("click", () => {
+    modal.classList.replace("hidden", "flex");
+  });
+};
