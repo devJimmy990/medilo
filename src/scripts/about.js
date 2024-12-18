956b79ebddc1cba813a3fa474e8c3a4c49223c6d
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2024-12-18 14:14:34 +0200

    update about page and add it`s own script

---
+++ src/scripts/about.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+
+    document.getElementById('btn-more-doctors')
+        .addEventListener('click', () => { window.location.assign('our-doctors.html'); });
+
+});
