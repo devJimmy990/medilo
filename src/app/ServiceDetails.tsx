dd99336574715068569a7f933e3a429131f3d226
Eslam Fayez <eslamfayez34@gmail.com>
2024-12-19 16:20:33 +0200

    Refactor service components and update ServiceDetails layout

--- src/app/ServiceDetails.tsx
+++ src/app/ServiceDetails.tsx
@@ -1,11 +1,19 @@
-import { memo } from 'react';
+import { memo } from "react";
+
+import AllService from "components/service-details/AllService";
+import ServiceDetailsText from "components/service-details/ServiceDetailsText";
+import ServiceDetailsBox from "components/service-details/ServiceDetailsBox";
+import ServiceDetailsIcons from "components/service-details/ServiceDetailsIcons";
 
 const ServiceDetails = () => {
-    return (
-        <div>
-
-        </div>
-    );
-}
+  return (
+    <main className="px-2 md:px-16 lg:px-24 xl:px-32 2xl:px-48 py-4">
+      <AllService />
+      <ServiceDetailsText />
+      <ServiceDetailsBox />
+      <ServiceDetailsIcons />
+    </main>
+  );
+};
 
 export default memo(ServiceDetails);
--- src/app/Services.tsx
+++ src/app/Services.tsx
@@ -1,11 +1,19 @@
-import { memo } from 'react';
+import { memo } from "react";
+
+import Boxes from "components/our-services/Boxes";
+import HealthCare from "components/our-services/HealthCare";
+import HighQuality from "components/our-services/HighQuality";
+import OurBestService from "components/our-services/OurBestService";
 
 const Services = () => {
-    return (
-        <div>
-
-        </div>
-    );
-}
+  return (
+    <main className="px-2 md:px-16 lg:px-24 xl:px-32 2xl:px-48 py-4">
+      <OurBestService />
+      <HighQuality />
+      <Boxes />
+      <HealthCare />
+    </main>
+  );
+};
 
 export default memo(Services);
