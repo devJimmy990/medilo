f5a657c2c1e49291f8d3a317f57d1be5d62d5c21
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2024-12-20 19:19:19 +0200

    update about page scores component

--- src/components/about/Scores.tsx
+++ src/components/about/Scores.tsx
@@ -1,43 +1,33 @@
-import { FC, memo } from 'react';
+import { FC, memo, ReactNode } from 'react';
+import { FaUserGroup, FaHeadset, FaChartLine, FaAward } from "react-icons/fa6";
 
+type ScoreModel = {
+    title: string,
+    score: string,
+    icon: ReactNode
+
+}
+const records: ScoreModel[] = [
+    { title: "Active Client", score: "567", icon: <FaUserGroup size={24} /> },
+    { title: "Team Support", score: "23K", icon: <FaHeadset size={24} /> },
+    { title: "Projects Complete", score: "241", icon: <FaChartLine size={24} /> },
+    { title: "Award Winner", score: "16K", icon: <FaAward size={24} /> },
+]
 const Scores: FC = () => {
     return (
-        <section className="container mx-auto grid grid-cols-2 lg:grid-cols-4 text-blue">
-            <article
-                className="flex flex-col justify-center items-center gap-2 border-[1.5px] border-gray p-12 text-center">
-                <div className="w-16 h-16 flex justify-center items-center rounded-full bg-blue text-white">
-                    <i className="fas fa-user-group fa-lg"></i>
-                </div>
-                <h4 className="text-2xl font-bold">567+</h4>
-                <p className="font-bold">Active Clients</p>
-            </article>
-
-            <article
-                className="flex flex-col justify-center items-center gap-2 border-[1.5px] border-gray p-12 text-center">
-                <div className="w-16 h-16 flex justify-center items-center rounded-full bg-blue text-white">
-                    <i className="fas fa-headset fa-lg"></i>
-                </div>
-                <h4 className="text-2xl font-bold">23K+</h4>
-                <p className="font-bold">Team Support</p>
-            </article>
-
-            <article
-                className="flex flex-col justify-center items-center gap-2 border-[1.5px] border-gray p-12 text-center">
-                <div className="w-16 h-16 flex justify-center items-center rounded-full bg-blue text-white">
-                    <i className="fas fa-chart-line fa-lg"></i>
-                </div>
-                <h4 className="text-2xl font-bold">241+</h4>
-                <p className="font-bold">Projects Completed</p>
-            </article>
-
-            <article
-                className="flex flex-col justify-center items-center gap-2 border-[1.5px] border-gray p-12 text-center">
-                <div className="w-16 h-16 flex justify-center items-center rounded-full bg-blue text-white">
-                    <i className="fas fa-award fa-lg text-white"></i>
-                </div>
-                <h4 className="text-2xl font-bold">16K+</h4>
-                <p className="font-bold">Award Winners</p>
-            </article>
+        <section className="container mx-auto grid grid-cols-2 lg:grid-cols-4 text-blue bg-white">
+            {
+                records.map(({ score, title, icon }) => (
+                    <article key={title}
+                        className="flex flex-col justify-center items-center gap-2 border-[1.5px] border-gray p-12 text-center">
+                        <div className="w-16 h-16 flex justify-center items-center rounded-full bg-blue text-white">
+                            {icon}
+                        </div>
+                        <h4 className="text-2xl font-bold">{score}+</h4>
+                        <p className="font-bold">{title}</p>
+                    </article>
+                ))
+            }
         </section>
     );
 }
