8a4f19c43c10270a23f28a4af88c6647af94a4f5
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2024-12-20 19:34:59 +0200

    add not found component and image

--- src/app/NotFound.tsx
+++ src/app/NotFound.tsx
@@ -2,8 +2,8 @@ import { memo } from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-
+        <div className='w-full h-screen flex items-center justify-center'>
+            <img src={require('../assets/images/not-found.jpg')} alt="not found" />
         </div>
     );
 }
