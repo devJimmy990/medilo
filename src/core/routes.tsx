4ccd8dd848b6ea2963014cc1a5ddc6d8075039d8
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2024-12-20 19:19:30 +0200

    update routes

--- src/core/routes.tsx
+++ src/core/routes.tsx
@@ -2,20 +2,35 @@ import { lazy, ReactNode } from "react";
 
 const Home = lazy(() => import("../app/Home"));
 const About = lazy(() => import("../app/About"));
+const Blogs = lazy(() => import("../app/Blogs"));
 const Contact = lazy(() => import("../app/Contact"));
 const Doctors = lazy(() => import("../app/Doctors"));
 const Services = lazy(() => import("../app/Services"));
+const Appointment = lazy(() => import("../app/Appointment"));
+const NotFound = lazy(() => import("../app/NotFound"));
 
-type Route = {
-    path: string,
-    element: ReactNode
-}
+type Route = { path: string, element: ReactNode }
+
 const routes: Route[] = [
     { path: "/", element: <Home /> },
+    { path: "/blogs", element: <Blogs /> },
     { path: "/about", element: <About /> },
     { path: "/contact", element: <Contact /> },
     { path: "/doctors", element: <Doctors /> },
     { path: "/services", element: <Services /> },
+    { path: "/appointment", element: <Appointment /> },
+    { path: "*", element: <NotFound /> }
 ];
 
+const Routes = Object.freeze({
+    Home: '/',
+    About: '/about',
+    Blogs: '/blogs',
+    Contact: '/contact',
+    Doctors: '/doctors',
+    Services: '/services',
+    Appointment: '/appointment',
+});
+
+export { Routes };
 export default routes;
